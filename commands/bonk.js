562200673a2e889e42ee33c4e0c8c8ae0729f3e6
squid <luisquid.dev@gmail.com>
2021-02-15 23:43:20 -0500

    Bonk command updated

--- commands/bonk.js
+++ commands/bonk.js
@@ -1,21 +1,40 @@
 const countGame = new Set();
 const { prefix } = require('../config');
 
+const Keyv = require('keyv');
+const keyv = new Keyv();
+
+keyv.on('error', err => console.error('Keyv connection error: ', err));
+
 exports.run = async (bot, message, args) => {
 
 	const cmdName = bot.commands.get('bonk', 'help.name');
 
 	const user = args[0];
-	if(!message.member.roles.cache.has('807758676206026802')) return message.channel.send("You are not part of the jorny prevention armed commando! òwó");
+	//if(!message.member.roles.cache.has('807758676206026802')) return message.channel.send("You are not part of the jorny prevention armed commando! òwó");
 	
 	if (!user) return message.channel.send(`Usage: \`${prefix + cmdName} <user>\``);
-	console.log(message.guild.member(user));
 	
 	const jorny = message.mentions.members.first();
-	console.log(jorny);
-	jorny.voice.setChannel('711407103125880914');
 
-	return message.channel.send(jorny.displayName + " has been bonked to jorny jail. òwó");
+	let faces = ["òwó", "( ͡° ͜ʖ ͡°)", "( ͡~ ͜ʖ ͡°)", "( ͡ʘ ͜ʖ ͡ʘ)", "ᕦ( ͡° ͜ʖ ͡°)ᕤ"]
+	
+	let bonks;
+	const bonkTimes = await keyv.get(jorny.id);
+	
+	if(!bonkTimes)
+	{
+		keyv.set(jorny.id, 1);
+		bonks = 1;
+	}
+	else
+	{
+		bonks = bonkTimes + 1;		
+	}
+
+	keyv.set(jorny.id, bonks);
+
+	return message.channel.send(jorny.displayName + " ha sido bonkeado " + bonks + (bonks == 1 ? " vez " : " veces") + faces[Math.floor(Math.random() * faces.length)]);
 
 };
 
